bf895fc6f3c22e0da524624d49c9aaec8bea7a7b
VOEUK <vithya.voeuk@cgi.com>
2018-05-03 11:42:59 +0200

    furet generation working

--- script.js
+++ script.js
@@ -15,20 +15,21 @@ $(".begin").on("click", function(){
 });
 
 function game_01(){
-	div.append(img);
-	$('.game_frame').append(div);
+	var imagen = new Image();
 
-	img.on("load", function(){
-	for(var i = 0; i < 10; i++){
-		//$(".game_frame").append($('<div class="game_floating_animal furet_'+ i +'"><img src="furet.png" /></div>'));
-		var img = $('<img src="furet.png" />');
-		var div = $('<div class="game_floating_animal furet_'+ i +'"></div>');
+	imagen.onload = function(){
+		var src_image_height = imagen.height;
+		var src_image_width = imagen.width;
 
-		
-			//var image_height = getRandomInt(50, 150);
+		for(var i = 0; i < 40; i++){
+			//$(".game_frame").append($('<div class="game_floating_animal furet_'+ i +'"><img src="furet.png" /></div>'));
+			var img = $('<img src="furet.png" />');
+			var div = $('<div class="game_floating_animal furet_'+ i +'"></div>');
 
-			var image_height = img.height();
-			var image_width = img.width();
+			var image_height = getRandomInt(50, 150);
+			var image_width = ( image_height * src_image_width ) / src_image_height;
+
+			
 			var window_height = $(".game_frame").height();
 			var window_width = $(".game_frame").width();
 
@@ -44,8 +45,17 @@ function game_01(){
 			div.css('margin-top', random_posy+"px");
 			div.css('margin-left', random_posx+"px");
 
-	}
-	});
+			div.append(img);
+			$('.game_frame').append(div);
+
+			img.css("width", image_width);
+			img.css("height", image_height);
+
+
+		}
+	};
+
+	imagen.src = "furet.png";
 
 			
 
